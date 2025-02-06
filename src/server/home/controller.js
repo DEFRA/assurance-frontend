e4a4adc594331c174367f6dd273b32224eb59a7b
calum-gunn1 <calum.douglas.gunn@gmail.com>
2025-02-06 19:37:24 +0000

    Initial commit

--- src/server/home/controller.js
+++ src/server/home/controller.js
@@ -1,13 +1,25 @@
+import { config } from '~/src/config/config.js'
+
 /**
  * A GDS styled example home page controller.
- * Provided as an example, remove or modify as required.
  * @satisfies {Partial<ServerRoute>}
  */
+
 export const homeController = {
-  handler(_request, h) {
+  handler: async (request, h) => {
+    let data
+    const response = await fetch(`${config.get('api.baseUrl')}/projects`)
+
+    if (response.ok) {
+      data = await response.json()
+    }
+
+    request.logger.info(response, 'Fetching projects from API')
+
     return h.view('home/index', {
-      pageTitle: 'Home',
-      heading: 'Home'
+      pageTitle: 'DDTS Technical Assurance Dashboard',
+      heading: 'DDTS Technical Assurance Dashboard',
+      projects: data
     })
   }
 }
